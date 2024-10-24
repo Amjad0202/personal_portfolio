206bb3a83588ff3d3a03af448059f1649c4fd939
Amjad <152121440+Amjad0202@users.noreply.github.com>
2024-10-24 07:39:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Use root for custom domains
+  base: '/personal_portfolio/',  // Set the base path for GitHub Pages
 });
